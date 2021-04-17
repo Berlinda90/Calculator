530f3e33f75b12dd4aaf9701a8874e3325784d4b
Berlinda Dolcy <75236642+Berlinda90@users.noreply.github.com>
2021-04-16 21:24:57 -0400

    Add files via upload

---
+++ Calculator.php
@@ -0,0 +1,19 @@
+<?php
+
+echo (15 + 3). "\n.";
+echo (21 - 3). "\n.";
+echo (23 * 4). "\n.";
+echo (44 / 3). "\n.";
+echo (23 % 9). "\n.";
+
+$myresults = 35; 
+
+{
+$myresults = 5 * 7;
+echo $myresults;
+}
+
+
+
+
+?>
